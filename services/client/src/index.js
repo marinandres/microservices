1cfb6a97270a5a74daeb90529f086d56e4440f5b
andres marin <marinandres698@gmail.com>
2018-08-20 12:54:36 -0500

    Status

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 class App extends Component {
   constructor() {
     super();
+    this.getUsers();
   }
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
