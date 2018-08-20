66e56b3cf67d7e173aae3a1e9dcd13be069d2808
andres marin <marinandres698@gmail.com>
2018-08-20 00:22:43 -0500

    Two developers

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';  // new
 import ReactDOM from 'react-dom';
-
+import axios from 'axios';
 
 // new
 class App extends Component {
   constructor() {
     super();
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); });
   }
   render() {
     return (
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,6 @@
 # services/users/project/__init__.py
 
-
+from flask_cors import CORS
 import os
 
 from flask import Flask  # new
@@ -17,12 +17,16 @@ def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
+    # enable CORS
+    CORS(app)  # new
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
