d51dafd94c7a8eceb1dc165b5610ce28de7bfc93
Guillermo Marin <marin.guillermo28@gmail.com>
2018-08-24 22:42:27 -0500

    React

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -27,11 +27,9 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
-<<<<<<< HEAD
     toolbar.init_app(app)  # new
-=======
+
     # toolbar.init_app(app)
->>>>>>> a25d4d2a68b6637a2685b2dba59376c901f5ce96
 
     # register blueprints
     from project.api.users import users_blueprint
