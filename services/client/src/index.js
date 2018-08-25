709d0f925c8c4586f9b5eda350f1e9d796b211d7
Guillermo Marin <marin.guillermo28@gmail.com>
2018-08-24 20:44:04 -0500

    Change made

---
+++ services/client/src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';  // new
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+// new
+class App extends Component {
+  constructor() {
+    super();
+    this.getUsers();
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); });
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,6 @@
 # services/users/project/__init__.py
 
-
+from flask_cors import CORS
 import os
 
 from flask import Flask
@@ -18,13 +18,20 @@ def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
+    # enable CORS
+    CORS(app)  # new
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     # set up extensions
     db.init_app(app)
+<<<<<<< HEAD
     toolbar.init_app(app)  # new
+=======
+    # toolbar.init_app(app)
+>>>>>>> a25d4d2a68b6637a2685b2dba59376c901f5ce96
 
     # register blueprints
     from project.api.users import users_blueprint
